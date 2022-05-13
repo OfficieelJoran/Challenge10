e57047dd2603ab72c4d44974689a3da5785257ad
OfficieelJoran <91888385+OfficieelJoran@users.noreply.github.com>
2022-05-13 12:53:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="stylechal10.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<header>
+
+    <p class="titel">
+    Cyber security </p>
+
+    <p class="gemaakt">
+    Gemaakt door Jurre en Joran</p>
+
+</header>
+<body>
+
+<ul class="navbar">
+    <a class="active" href="index.php">Risico's</a>
+    <a class="nav" href="omgangpage.php">Omgang Gegevens</a>
+    <a class="nav" href="">Home</a>
+    <a class="nav" href="">Gegevens beveiligen</a>
+</ul>
+
+<img src="jup2.jpg" class="headerfoto">
+
+<div class="mainvak1">
+    <div class="mainvak1back"></div>
+    <p4>Risico's van online internetten</p4>
+    <div class="secondaryvakborder">
+            <div class="secondaryvakback">
+                <div class="thirdvakback"></div>
+            </div>
+            
+    
+    <p1>☼ Welke risico's zijn er bij het gebruik van internet? </p1> <br> <br>
+
+    <p2>
+    -Virussen <br>
+    -Malware <br>
+    -Spyware <br>
+    -Ransomware <br>
+    -Phising <br>
+    -Spam <br>
+    -Botnet <br>
+    -DDos <br>
+    -Identiteitsdiefstal <br>
+    -Trojans <br>
+    -Onbeveiligd netwerk <br> </p2>  </div>
+
+    <div class="secondaryvakborder2">
+        <div class="secondaryvakback">
+            <div class="thirdvakback">
+            </div>
+        </div>
+
+    <p1>Virussen</p1> 
+
+    <p2>
+
+    Wat zijn virussen? <br>
+    Virussen zijn kleine programma's die zich verstoppen in andere bestanden,
+    bijvoorbeeld foto's of bestanden die u per mail ontvangt. 
+    Eenmaal op uw computer kunnen ze flinke schade aanrichten door bijvoorbeeld bestanden te beschadigen of verwijderen. <br>
+    </p2> 
+
+    <p3> Malware </p3> 
+
+    <p2>
+    Wat is malware? <br>
+
+    Malware is de overkoepelende term voor computersoftware of code met kwaadaardige bedoelingen.
+    Malware probeert computers, computersystemen, netwerken en/of mobiele apparaten binnen te dringen,
+    te beschadigen of uit te schakelen, vaak door (gedeeltelijk) de controle over te nemen over het apparaat. </p2>
+
+
+    </div>
+</div>
+
+<div class="mainvak1">
+    <div class="mainvak1back"></div>
+    <div class="secondaryvakborder3">
+            <div class="secondaryvakback">
+                 <div class="thirdvakback">
+                 </div>
+            </div>
+    
+    <p1> Spyware </p1> <br> <br>
+
+    <p2>
+    Wat is spyware? <br>
+    Spyware is software die zich ongezien op de computer nestelt om internetgedrag in de gaten te houden.
+    Deze informatie wordt gebruikt om gerichte spam te versturen of uw persoonlijke gegevens, bijvoorbeeld bankrekeningnummers,
+    te verkopen of op andere wijze te misbruiken. Vaak krijgt u ook veel ongewenste reclame via pop-ups. </p2>  </div>
+
+
+    <div class="secondaryvakborder4">
+        <div class="secondaryvakback">
+            <div class="thirdvakback">
+            </div>
+        </div>
+
+    <p1>Ransomware</p1> 
+
+    <p2>
+
+    Wat is Ransomware? <br>
+    Ransomware is een online chantagemethode door middel van malware.
+    Ransomware is een programma dat een computer (of gegevens die erop staan) blokkeert en vervolgens van de gebruiker losgeld vraagt om de computer weer te 'bevrijden'. <br>
+    </p2> 
+
+    <p3> Phishing </p3> 
+
+    <p2>
+    Wat is Phishing? <br>
+
+    Phishing is een verzamelnaam voor e-mail of internetpagina's die zich voordoen als vertrouwde instellingen en bedrijven,
+    vaak uw bank of veilingsites, maar dat in werkelijkheid niet zijn. Ze zullen u vragen om wachtwoorden of
+    een creditcardnummer te geven om die vervolgens te misbruiken en geld of gegevens van u te stelen. </p2>
+
+
+    </div>
+</div>
+   
+<div class="mainvak1">
+    
+    
+
+    <div class="mainvak1back"></div>
+    <div class="vtmbetekenis">
+    ((*VTM)<br> Het gaat om VTM Go, dat vernoemd is naar de commerciële televisiezenders in België.
+    De dienst is helemaal gratis en ook in Nederland te gebruiken. Er zijn zowel Vlaamse als internationale films en series te zien. 
+    VTM Go is beschikbaar op het web, maar ook als app voor iOS en Android.)  <br> <br> <br>
+
+    </div> 
+    
+
+
+    <div class="secondaryvakborder3">
+            <div class="secondaryvakback">
+                <div class="thirdvakback"> 
+                </div>
+            </div>
+    
+    <p1>Spam</p1> <br> <br>
+
+    <p2>
+        Wat is spam? <br> 
+    
+        Spam is ongewenste e-mail die erop gericht is om u een product of dienst te verkopen, zonder dat u er om gevraagd hebt. </p2>
+
+    <p3> Botnet </p3>
+
+    <p2> Wat is een Botnet? <br>
+    Wat is een botnet?
+    Een botnet is een groot aantal computers die allemaal door een virus zijn geïnfecteerd.
+    Botnets worden door criminelen gebruikt om, zonder dat de gebruikers het weten, spam of virussen te versturen.
+     Vaak hebt u als gebruiker niet door dat uw computer deel uitmaakt van een botnet. *VTM houdt zijn netwerk actief in de gaten om botnets te voorkomen. <br>
+    </p2>
+
+
+    </div>
+
+
+
+    <div class="secondaryvakborder4">
+            <div class="secondaryvakback">
+                <div class="thirdvakback">
+                </div>
+            </div>
+
+    <p1>DDoS</p1> 
+
+    <p2>
+
+    Wat is een DDos aanval? <br>
+    Distributed denial-of-service aanvallen (DDoS-aanvallen) zijn pogingen om een computer, computernetwerk of dienst onbruikbaar te maken voor de bedoelde gebruiker.
+    Bij een distributed dos-aanval voeren meerdere computers tegelijk de aanval online uit.
+    Hiervoor wordt vaak een botnet gebruikt, maar het kan ook gaan om meerdere personen die hun acties coördineren. <br>
+    </p2> 
+
+    <p3> Hackers </p3> 
+
+    <p2>
+    wat zijn hackers? <br>
+
+    Malware is de overkoepelende term voor computersoftware of code met kwaadaardige bedoelingen.
+    Malware probeert computers, computersystemen, netwerken en/of mobiele apparaten binnen te dringen,
+    te beschadigen of uit te schakelen, vaak door (gedeeltelijk) de controle over te nemen over het apparaat. </p2>
+
+
+    </div>
+</div>
+
+<div class="mainvak1">
+    <div class="mainvak1back"></div>
+    <div class="secondaryvakborder3">
+            <div class="secondaryvakback">
+                <div class="thirdvakback">
+                </div>
+            </div>
+    
+            <p1>Identiteitsdiefstal</p1> 
+
+    <p2>
+
+    Wat is Identiteitsdiefstal? <br>
+    Identiteitsdiefstal is het stelen van persoonlijke gegevens via internet.
+    Denk hierbij aan paspoortnummers, e-mailadressen, creditcardgegevens en tan- of pincodes voor internetbankieren.
+    Met deze informatie kan de dief vervolgens online aankopen doen,
+    of de informatie doorverkopen aan derden die het vervolgens gebruiken voor spam of andere criminele activiteiten. 
+    Identiteitsdiefstal vindt vaak plaats door eerst een computer te infecteren met een virus of Trojaans paard om zo aan de gegevens te komen. <br>
+    </p2>   </div>
+
+
+    <div class="secondaryvakborder4">
+                <div class="secondaryvakback">
+                    <div class="thirdvakback">
+                    </div>
+                </div>
+
+    <p1>Trojans</p1> 
+
+    <p2>
+
+    Wat zijn Trojans? <br>
+    Een trojan is schadelijke software vermomd als een betrouwbaar programma.
+    Als gebruiker denk je een betrouwbaar programma op je computer te installeren, 
+    maar in werkelijkheid krijgt een crimineel daarmee toegang tot jouw computer. <br>
+    </p2> 
+
+    <p5> Onbeveiligd draadloos netwerk </p5> 
+
+    <p2>
+    Wat is Phishing? <br>
+
+    Veel mensen maken op kantoor en thuis gebruik van een draadloos netwerk.
+    Het is belangrijk dat u deze internetverbinding goed beveiligt zodat anderen geen
+    gebruik kunnen maken van uw internettoegang of via deze verbinding kunnen inbreken in uw computer. </p2>
+
+
+    </div>
+</div>
+
+<div class="bronvermelding">
+&nbsp;&nbsp;&nbsp; Bronnen die gebruikt zijn: <br> <br>
+    Betekenis VTM <br> Bron = <a href="https://www.onemorething.nl" target="_blank"> onemorething.nl  </a><br> <br>
+    Informatie van deze pagina <br> Bron = <a href="https://www.vtmgroep.nl/kennisdossiers/it-security/gevaren-op-het-internet" target="_blank">vtmgroep.nl
+</div>
+
+
+</body>
+</html>
---
+++ omgangpage.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="stylechal10.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<header>
+
+    <p class="titel">
+    Cyber security </p>
+
+    <p class="gemaakt">
+    Gemaakt door Jurre en Joran</p>
+
+</header>
+<body>
+
+<ul class="navbar">
+    <a class="nav" href="index.php">Risico's</a>
+    <a class="active" href="omgangpage.php">Omgang Gegevens</a>
+    <a class="nav" href="">Home</a>
+    <a class="nav" href="">Gegevens beveiligen</a>
+</ul>
+
+<img src="jup2.jpg" class="headerfoto">
+
+<div class="allevakken">
+
+<div class="mainvak1omgang">
+    <div class="mainvak1backomgang"></div>
+    <p4>Persoonsgegevens op internet</p4>
+    <div class="secondaryvakborderomgang">
+            <div class="secondaryvakbackomgang">
+                <div class="thirdvakbackomgang"></div>
+            </div>
+            
+    <br>
+
+    <p2>
+    Veel mensen publiceren gegevens over zichzelf of over anderen op internet,
+    zoals foto’s op Facebook, Instagram of Snapchat. Ook organisaties plaatsen persoonsgegevens op internet.
+    De gevolgen hiervan kunnen groot zijn voor de mensen om wie het gaat. Onder meer omdat eenmaal op internet geplaatste gegevens jaren later nog vindbaar zijn.
+    Dit kan bijvoorbeeld bij een sollicitatie nadelige gevolgen hebben. </div> </div>
+
+    <div class="mainvak1omgang">
+    <div class="mainvak1backomgang"></div>
+    <p4>Toestemming</p4>
+    <div class="secondaryvakborderomgang">
+            <div class="secondaryvakbackomgang">
+                <div class="thirdvakbackomgang"></div>
+            </div>
+            
+    <br>
+
+    <p2>
+    Niemand mag zomaar persoonsgegevens van een ander op internet publiceren.
+    Dit mag in principe alleen als daarvoor een wettelijke grondslag is uit de Algemene verordenings gegevensbescherming (AVG)
+    In de AVG staan *zes grondslagen*. Eén daarvan is toestemming.
+    Vaak is om persoonsgegevens te publiceren op internet toestemming nodig van de mensen om wie het gaat. Maar niet altijd.
+    Er geld bijvoorbeeld een uitzondering voor persoonlijk of huishoudelijk gebruik. In dat geval is de AVG niet van toepassing. </div> </div>
+
+
+    <div class="mainvak1omgang">
+    <div class="mainvak1backomgang"></div>
+    <p4>Persoongegevens verwijderen</p4>
+    <div class="secondaryvakborderomgang">
+            <div class="secondaryvakbackomgang">
+                <div class="thirdvakbackomgang"></div>
+            </div>
+            
+    <br>
+
+    <p2>
+    Staan iemands gegevens op internet, dan heeft diegene het recht om deze gegevens te laten verwijderen.
+    Bijvoorbeeld omdat ze niet langer nodig zijn voor het doel waarvoor ze verzameld of verwerkt zijn,
+    of omdat deze persoon niet wilt dat zijn/haar gegevens te vinden zijn door onbekenden. </div> </div>
+
+</div>
+
+
+</body>
